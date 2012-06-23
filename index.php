368c0fc7fd4a6cb5c3d719a1889649cec4fa10ea
Tauraloke Esteru <tauraloke@gmail.com>
2012-06-23 22:12:27 +0400

    inspect SERVER

--- index.php
+++ index.php
@@ -1,2 +1,35 @@
 <?php
-phpinfo();
+
+# Demo for Dragon's Nest OAuth
+
+define('CLIENT_ID', 'c44e396fb98e8e7f6555911fd828fa43');
+define('CLIENT_SECRET', '0d7a0182b5297ae66d9f4b62d5bff957');
+
+
+function sendQuery($query)
+{
+	$curl_handle=curl_init();
+	curl_setopt($curl_handle, CURLOPT_URL, $query);
+	curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 30);
+	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl_handle, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl_handle, CURLOPT_USERAGENT, 'OAuth bot');
+	$response = curl_exec($curl_handle);
+	curl_close($curl_handle);
+	$obj = json_decode($response);
+	if($response and !$obj)
+	{
+		echo $response;
+		exit;
+	}
+	return $obj;
+}
+
+print_r($_SERVER);exit;
+if(empty($_COOKIE['uid']))
+{
+	header('Location: http://dragons-nest.ru/forum/oauth.php?query=auth&redirect_uri='.$redirect_uri.'&client_id='.CLIENT_ID);
+}
+var_dump(sendQuery('http://dragons-nest.ru/forum/oauth.php?query=auth&redirect_uri={link}&client_id={client_id}'));
+
+
